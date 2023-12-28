3100759227e50feb4b6b67eaf2e464809c663c58
Mark McDermott <mark@markmcdermott.io>
2023-12-28 10:07:01 -0600

    Add model tests in document_spec

--- install.sh
+++ install.sh
@@ -3194,6 +3194,35 @@ RSpec.describe "/documents", type: :request do
 end
 ~
 EOF
+cat <<'EOF' | puravida spec/models/document_spec.rb ~
+require 'rails_helper'
+
+RSpec.describe Document, type: :model do
+  fixtures :users, :cars, :maintenances, :documents
+  let(:valid_attributes) {{ 
+    date: Date.parse("20200713"),
+    name: "name",
+    notes: "notes",
+    documentable_type: "Maintenance",
+    documentable_id: maintenances(:fiat_alignment).id
+  }}
+  let(:invalid_attributes) {{ 
+    date: Date.parse("20200713"),
+    name: "name",
+    notes: "notes",
+    documentable_type: "Maintenance",
+    documentable_id: -1
+  }}
+
+  it "is valid with valid attributes" do
+    expect(Document.new(valid_attributes)).to be_valid
+  end
+  it "is not valid width poorly formed email" do
+    expect(Document.new(invalid_attributes)).to_not be_valid
+  end
+end
+~
+EOF
 rspec
 
 # echo -e "\n\n🦄 FRONTEND\n\n"
