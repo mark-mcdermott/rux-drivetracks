68ea5cad767040d216f65d235ee35b0cab21ba63
Mark McDermott <mark@markmcdermott.io>
2023-12-29 11:26:03 -0600

    Add getNewDocUrl to documents/index.vue

--- install.sh
+++ install.sh
@@ -4474,12 +4474,32 @@ cat <<'EOF' | puravida pages/documents/index.vue ~
 <template>
   <main class="container">
     <h1>Documents</h1>
-    <NuxtLink to="/documents/new" role="button">Add Document</NuxtLink>
+    <NuxtLink :to="`${newDocUrl}`" role="button">Add Document</NuxtLink>
     <DocumentSet />
   </main>
 </template>
 <script>
-export default { middleware: 'currentOrAdmin-index' }
+  export default { 
+    middleware: 'currentOrAdmin-index',
+    data () {
+      return {
+        newDocUrl: ""
+      }
+    },
+    mounted() {
+      this.newDocUrl = this.getNewDocUrl()
+    },
+    methods: {
+      getNewDocUrl: () => {
+        const url = "/documents/new"
+        let query = ""
+        if ($nuxt.$route.query && $nuxt.$route.query.user_id) {
+          query = `?user_id=${$nuxt.$route.query.user_id}`
+        }
+        return url + query
+      }
+    }
+  }
 </script>
 ~
 EOF
