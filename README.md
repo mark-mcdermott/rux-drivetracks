ff234d071f31713dd9c681304b031376ab2d6bec
Mark McDermott <mark@markmcdermott.io>
2023-12-30 01:33:40 -0600

    Add cars/maintenances/documents to admin page

--- README.md
+++ README.md
@@ -5488,8 +5488,13 @@ export const getters = {
     <h1>Admin</h1>
     <p>Number of users: {{ this.users.length }}</p>
     <p>Number of admins: {{ (this.users.filter((obj) => obj.admin === true)).length }}</p>
+    <p>Number of cars: {{ this.cars.length }}</p>
+    <p>Number of maintenances: {{ this.maintenances.length }}</p>
+    <p>Number of documents: {{ this.documents.length }}</p>
     <p><NuxtLink to="/users">Users</NuxtLink></p>
     <p><NuxtLink to="/cars?admin=true">Cars</NuxtLink></p>
+    <p><NuxtLink to="/maintenances?admin=true">Maintenances</NuxtLink></p>
+    <p><NuxtLink to="/documents?admin=true">Documents</NuxtLink></p>
   </main>
 </template>
 
@@ -5497,8 +5502,18 @@ export const getters = {
 export default { 
   middleware: 'adminOnly',
   layout: 'admin',
-  data: () => ({ users: [] }),
-  async fetch() { this.users = await this.$axios.$get('users') }
+  data: () => ({ 
+    users: [],
+    cars: [],
+    maintenances: [],
+    documents: []
+  }),
+  async fetch() { 
+    this.users = await this.$axios.$get('users')
+    this.cars = await this.$axios.$get('cars')
+    this.maintenances = await this.$axios.$get('maintenances')
+    this.documents = await this.$axios.$get('documents')
+  }
 }
 </script>
 ~
--- install.sh
+++ install.sh
@@ -5040,8 +5040,13 @@ cat <<'EOF' | puravida pages/admin/index.vue ~
     <h1>Admin</h1>
     <p>Number of users: {{ this.users.length }}</p>
     <p>Number of admins: {{ (this.users.filter((obj) => obj.admin === true)).length }}</p>
+    <p>Number of cars: {{ this.cars.length }}</p>
+    <p>Number of maintenances: {{ this.maintenances.length }}</p>
+    <p>Number of documents: {{ this.documents.length }}</p>
     <p><NuxtLink to="/users">Users</NuxtLink></p>
     <p><NuxtLink to="/cars?admin=true">Cars</NuxtLink></p>
+    <p><NuxtLink to="/maintenances?admin=true">Maintenances</NuxtLink></p>
+    <p><NuxtLink to="/documents?admin=true">Documents</NuxtLink></p>
   </main>
 </template>
 
@@ -5049,8 +5054,18 @@ cat <<'EOF' | puravida pages/admin/index.vue ~
 export default { 
   middleware: 'adminOnly',
   layout: 'admin',
-  data: () => ({ users: [] }),
-  async fetch() { this.users = await this.$axios.$get('users') }
+  data: () => ({ 
+    users: [],
+    cars: [],
+    maintenances: [],
+    documents: []
+  }),
+  async fetch() { 
+    this.users = await this.$axios.$get('users')
+    this.cars = await this.$axios.$get('cars')
+    this.maintenances = await this.$axios.$get('maintenances')
+    this.documents = await this.$axios.$get('documents')
+  }
 }
 </script>
 ~
