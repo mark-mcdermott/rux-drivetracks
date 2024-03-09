16ade0baf3d60fc890926493ca02ffca59ad1bd3
Mark McDermott <mark@markmcdermott.io>
2024-03-09 09:07:04 -0600

    Fix sign up test (hopefully)

--- install.sh
+++ install.sh
@@ -5216,6 +5216,7 @@ cat <<'EOF' | puravida cypress/e2e/sign-up-flow.cy.js ~
 // run dev server with test db: CYPRESS=1 bin/rails server -p 3000
 describe('Sign Up Flow', () => {
   it('Should redirect to user show page', () => {
+    let userId = -1
     cy.visit('http://localhost:3001/sign-up')
     cy.get('p').contains('Name').next('input').type('name')
     cy.get('p').contains('Email').next('input').type('test' + Math.random().toString(36).substring(2, 15) + '@mail.com')
@@ -5225,8 +5226,19 @@ describe('Sign Up Flow', () => {
     cy.get('button').contains('Create User').click()
     cy.url().should('match', /http:\/\/localhost:3001\/users\/\d+/)
     cy.get('h2').should('contain', 'name')
-    // TODO: assert avatar presence
-    // cy.logout()
+    cy.get('p').contains('avatar:').next('img').should('have.attr', 'src').should('match', /http.*dwight-schrute.png/)
+    cy.url().then(url => {
+      userId = url.split('/')[url.split('/').length - 1]
+      cy.logout()
+      cy.loginAdmin()
+      cy.visit('http://localhost:3001/users/' + userId)
+      cy.url().should('match', /http:\/\/localhost:3001\/users/`${userId}`)
+      cy.get('h2').children().eq(2).click() // deletes user
+      cy.visit('http://localhost:3001/users/')
+      cy.url().should('match', /http:\/\/localhost:3001\/users/)
+      cy.get('section').children('div').should('have.length', 3)
+      cy.logout()
+    })    
   })
 })
 ~
