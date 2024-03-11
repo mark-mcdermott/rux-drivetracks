289d7e92f8d384621d91027918e2f2749ee0499d
keeganoshea <koshea@doximity.com>
2024-03-10 22:26:54 -0600

    Use build_stubbed instead of create https://thoughtbot.com/blog/use-factory-bots-build-stubbed-for-a-faster-test

--- back/spec/models/car_spec.rb
+++ back/spec/models/car_spec.rb
@@ -3,7 +3,7 @@
 require 'rails_helper'
 
 RSpec.describe Car, type: :model do
-  let(:car) { create(:car) }
+  let(:car) { build_stubbed(:car) }
 
   describe "relationships" do
     it { is_expected.to belong_to(:user) }
