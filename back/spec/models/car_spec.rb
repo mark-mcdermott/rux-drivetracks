37115bee467251fd45c9a2d9489f61f1af9533b1
keeganoshea <koshea@doximity.com>
2024-03-10 22:15:57 -0600

    Refactor to use belong_to

--- back/spec/models/car_spec.rb
+++ back/spec/models/car_spec.rb
@@ -5,6 +5,10 @@ require 'rails_helper'
 RSpec.describe Car, type: :model do
   let(:car) { create(:car) }
 
+  describe "relationships" do
+    it { is_expected.to belong_to(:user) }
+  end
+
   it 'is valid with valid attributes' do
     expect(car).to be_valid
   end
@@ -16,11 +20,6 @@ RSpec.describe Car, type: :model do
 
   it 'is not valid when name too long' do
     car.name = 'c' * 255
-    expect(car).not_to be_valid
-  end
-
-  it 'is not valid without a user' do
-    car.user = nil
     expect(car).not_to be_valid
   end
 end
