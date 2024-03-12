f1cc124e9e07764aa18b45057e56dc87188ae5f5
keeganoshea <koshea@doximity.com>
2024-03-11 20:36:20 -0600

    Run generator rails g rswag:install

--- back/config/routes.rb
+++ back/config/routes.rb
@@ -1,6 +1,8 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
+  mount Rswag::Ui::Engine => '/api-docs'
+  mount Rswag::Api::Engine => '/api-docs'
   resources :documents
   resources :users
   resources :cars
