e9ff32a4998636c18c7fd851da9d9f7538b7e502
Mark McDermott <mark@markmcdermott.io>
2024-03-21 01:28:18 -0500

    Add custom homepage

--- front/pages/index.vue
+++ front/pages/index.vue
@@ -1,31 +1,8 @@
 <template>
   <main class="container">
-    <h1>Rails 7 Nuxt 2 Admin Boilerplate</h1>
-    
-    <h2 class="small-bottom-margin">Features</h2>
-    <ul class="features">
-      <li>Admin dashboard</li>
-      <li>Placeholder users</li>
-      <li>Placeholder user item ("car")</li>
-    </ul>
-
-    <h3 class="small-bottom-margin stack">Stack</h3>
-    <div class="aligned-columns">
-      <p><span>frontend:</span> Nuxt 2</p>
-      <p><span>backend API:</span> Rails 7</p>
-      <p><span>database:</span> Postgres</p>
-      <p><span>styles:</span> Sass</p>
-      <p><span>css framework:</span> Pico.css</p>
-      <p><span>e2e tests:</span> Cypress</p>
-      <p><span>api tests:</span> RSpec</p>
-    </div>
-
-    <h3 class="small-bottom-margin tools">Tools</h3>
-    <div class="aligned-columns">
-      <p><span>user avatars:</span> local active storage</p>
-      <p><span>backend auth:</span> bcrypt & jwt</p>
-      <p><span>frontend auth:</span> nuxt auth module</p>
-    </div>
+    <h1>Drivetracks</h1>
+    <p class="subtitle">Cloud Car Document Storage</p>
+    <img class="challenger" :src="require(`@/assets/images/challenger.png`)" />
 
     <h3 class="small-bottom-margin">User Logins</h3>
     <table class="half-width">
@@ -39,7 +16,6 @@
       <NuxtLink to="/log-in" role="button" class="secondary">Log In</NuxtLink> 
       <NuxtLink to="/sign-up" role="button" class="contrast outline">Sign Up</NuxtLink>
     </p>    
-
   </main>
 </template>
 
